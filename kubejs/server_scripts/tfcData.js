fc754b5811e7d02dce8eec00f1e78b70416969a4
Pablo J Blasco <pablojblasco@gmail.com>
2026-07-31 17:53:37 +0200

    Allow redstone_imbued_quartz_powder to enter crucibles and such

--- kubejs/server_scripts/tfcData.js
+++ kubejs/server_scripts/tfcData.js
@@ -1,4 +1,5 @@
 TFCEvents.data( event => {
     event.heat({ ingredient: { item: 'tfmg:bauxite' }, heat_capacity: 13.333, forging_temperature: 396, welding_temperature: 528 })
     event.heat({ ingredient: { item: 'tfmg:galena' }, heat_capacity: 18.182, forging_temperature: 197, welding_temperature: 262 })
+    event.heat({ ingredient: { item: 'kubejs:redstone_imbued_quartz_powder' }, heat_capacity: 0.7 })
 })
